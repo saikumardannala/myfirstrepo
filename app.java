e3a1bf7337af03f13961982db367ba41bf298f71
EC2 Default User <ec2-user@ip-172-31-22-104.ec2.internal>
2025-07-29 12:46:45 +0000

    code added by me

---
+++ app.java
@@ -0,0 +1,14 @@
+public class Greeting {
+
+    // Instance variable
+    private String name;
+
+    // Constructor
+    public Greeting(String name) {
+        this.name = name;
+    }
+
+    // Method to greet the user
+    public void sayHello() {
+        System.out.println("Hello, " + name +
+
